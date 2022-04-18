6cb16209af371a51bbf2744d1a546bbf7ca94eab
Vitor Dias <vitor_dias@msn.com>
2022-04-18 19:39:59 -0300

    Subindo em ritmo de festa maooooeee Direto na main que hoje é dia de maldade

---
+++ FirstCode/Condicional/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Condicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //podemos chamar variaveis de qualquer nome, inclusive batata
+            string batata = Console.ReadLine();
+
+            #region "Exemplo condicional com AND e OR"
+            //Criação da primeira condicional se libera ou não empréstimos
+            int salario;
+            int emprestimo;
+            bool nomeSujo = false;
+            Console.WriteLine("Digite o seu salário:");
+            Console.WriteLine("Digite o valor do empréstimo:");
+            int.TryParse(Console.ReadLine(), out salario);
+            int.TryParse(Console.ReadLine(), out emprestimo);
+
+            //Condicional utilizando o || (OR)
+            if (nomeSujo || (emprestimo > (salario * 0.2)))
+            {
+                Console.WriteLine("Empréstimo não concedido");
+            }
+            else
+            {
+                Console.WriteLine("Empréstimo concedido");
+            }
+
+            //Condicional utilizando o && (AND)
+            if (!(emprestimo > (salario * 0.2)) & !nomeSujo)
+            {
+                Console.WriteLine("Empréstimo concedido");
+            }
+            else
+            {
+                Console.WriteLine("Empréstimo não concedido");
+            }
+            #endregion
+
+            #region "Condicional mais complexo, usando else if ou switch"
+
+            #region "Else IF"
+
+            Console.WriteLine("Digite um dia da semana de 1 a 7");
+            int diaSemana;
+            if (!int.TryParse(Console.ReadLine(), out diaSemana))
+            {
+                Console.WriteLine("Número inválido");
+                return;
+            }
+
+            if (diaSemana == 7)
+            {
+                Console.WriteLine();
+            }
+            else if (diaSemana == 1)
+            {
+                Console.WriteLine("O dia digitado foi Domingo");
+            }
+            else if (diaSemana == 2)
+            {
+                Console.WriteLine("O dia digitado foi Segunda");
+            }
+            else if (diaSemana == 3)
+            {
+                Console.WriteLine("O dia digitado foi Terça");
+            }
+            else if (diaSemana == 4)
+            {
+                Console.WriteLine("O dia digitado foi Quarta");
+            }
+            else if (diaSemana == 5)
+            {
+                Console.WriteLine("O dia digitado foi Quinta");
+            }
+            else if (diaSemana == 6)
+            {
+                Console.WriteLine("O dia digitado foi Sexta");
+            }
+            else
+            {
+                Console.WriteLine("Dia inválido");
+            }
+            #endregion
+
+            #region "Switch 2.0 Enhanced Version plus ultra interpolação "
+
+            string diaPorExtenso = "";
+            switch (diaSemana)
+            {
+                case 1:
+                    diaPorExtenso = "Domingo";
+                    break;
+                case 2:
+                    diaPorExtenso = "Segunda";
+                    break;
+                case 3:
+                    diaPorExtenso = "Terça";
+                    break;
+                case 4:
+                    diaPorExtenso = "Quarta";
+                    break;
+                case 5:
+                    diaPorExtenso = "Quinta";
+                    break;
+                case 6:
+                    diaPorExtenso = "Sexta";
+                    break;
+                case 7:
+                    diaPorExtenso = "Sábado";
+                    break;
+                default:
+                    Console.WriteLine("Dia inválido");
+                    return;
+            }
+            Console.WriteLine($"O número digitado foi {diaSemana} que equivale a {diaPorExtenso}");
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ FirstCode/FirstCode/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FirstCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "TODOS PASSARAM!";
+
+            int primeiroNumero = 1;
+            int segundoNumero = 2;
+
+            int resultado = primeiroNumero + segundoNumero;
+
+            decimal numeroDecimal = decimal.MaxValue;
+
+            DateTime dtAgora = DateTime.Now;
+
+            bool estaChovendo = true;
+
+            //Comando para mostrar mensagem no console
+            Console.WriteLine(msg);
+            Console.WriteLine(primeiroNumero);
+            Console.WriteLine(segundoNumero);
+            Console.WriteLine(resultado);
+            Console.WriteLine(numeroDecimal);
+            Console.WriteLine(dtAgora);
+            Console.WriteLine(estaChovendo);
+
+            Console.WriteLine("Digite um caracter");
+
+            //Comando para leitura de valor digitado no console
+            string inputUsuario = "";
+            inputUsuario = Console.ReadLine();
+
+            Console.WriteLine("O caracter digitado foi: " +  inputUsuario);
+
+
+        }
+    }
+}
